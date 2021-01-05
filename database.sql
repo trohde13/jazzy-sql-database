5984babc0f0fda7557f23779b907d8d7dfeefbb7
Thomas Rohde <trohde@rocketmail.com>
2021-01-05 17:36:11 -0600

    updated database.sql, added pg and pool, wrote GET and POST for /artist

--- database.sql
+++ database.sql
@@ -1,5 +1,12 @@
-CREATE TABLE "artists" (
-    "id" SERIAL PRIMARY KEY,
-    "artist_name" varchar(80) not null,
-    "year_born" date
+CREATE TABLE "artist" (
+	"id" SERIAL PRIMARY KEY,
+	"name" VARCHAR(80) NOT NULL,
+	"birthdate" DATE
+);
+
+CREATE TABLE "song" (
+	"id" SERIAL PRIMARY KEY,
+	"title" VARCHAR(255) NOT NULL,
+	"length" VARCHAR(10) NOT NULL,
+	"released" DATE
 );
--- server/server.js
+++ server/server.js
@@ -1,6 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+//setting up pg and Pool
+const pg = require('pg');
+
+const Pool = pg.Pool;
+
+const pool = new Pool({
+    database: 'jazzy_sql',
+    host: 'localhost',
+    port: 5432,
+});
+
+pool.on('connect', () => {
+    console.log('PG CONNECTED');
+});
+
+pool.on('error', (error) => {
+    console.log(error);
+});
+
+
 const app = express();
 const PORT = 5000;
 
@@ -12,50 +32,73 @@ app.listen(PORT, () => {
 });
 
 // TODO - Replace static content with a database tables
-const artistList = [ 
-    {
-        name: 'Ella Fitzgerald',
-        birthdate: '04-25-1917'
-    },
-    {
-        name: 'Dave Brubeck',
-        birthdate: '12-06-1920'
-    },       
-    {
-        name: 'Miles Davis',
-        birthdate: '05-26-1926'
-    },
-    {
-        name: 'Esperanza Spalding',
-        birthdate: '10-18-1984'
-    },
-]
-const songList = [
-    {
-        title: 'Take Five',
-        length: '5:24',
-        released: '1959-09-29'
-    },
-    {
-        title: 'So What',
-        length: '9:22',
-        released: '1959-08-17'
-    },
-    {
-        title: 'Black Gold',
-        length: '5:17',
-        released: '2012-02-01'
-    }
-];
+// const artistList = [ 
+//     {
+//         name: 'Ella Fitzgerald',
+//         birthdate: '04-25-1917'
+//     },
+//     {
+//         name: 'Dave Brubeck',
+//         birthdate: '12-06-1920'
+//     },       
+//     {
+//         name: 'Miles Davis',
+//         birthdate: '05-26-1926'
+//     },
+//     {
+//         name: 'Esperanza Spalding',
+//         birthdate: '10-18-1984'
+//     },
+// ]
+// const songList = [
+//     {
+//         title: 'Take Five',
+//         length: '5:24',
+//         released: '1959-09-29'
+//     },
+//     {
+//         title: 'So What',
+//         length: '9:22',
+//         released: '1959-08-17'
+//     },
+//     {
+//         title: 'Black Gold',
+//         length: '5:17',
+//         released: '2012-02-01'
+//     }
+// ];
 
 app.get('/artist', (req, res) => {
-    console.log(`In /songs GET`);
-    res.send(artistList);
-});
+    console.log(`In /artist GET`);
+
+    const queryText = `SELECT * FROM "artist" ORDER BY "birthdate";`
+    pool.query(queryText).then((result) => {
+        console.log(result);
+        res.send(result.rows);
+    }).catch((error) => {
+        console.log(error);
+        res.sendStatus(500);
+    })
+    
+
+}); //end app.get /artist
 
 app.post('/artist', (req, res) => {
-    artistList.push(req.body);
-    res.sendStatus(201);
+    //artistList.push(req.body);
+    console.log(req.body);
+    const queryText = `
+    INSERT INTO "artist" ("name", "birthdate")
+    VALUES ($1, $2);`
+    //res.sendStatus(201);
+
+    pool.query(queryText, [req.body.name, req.body.birthdate])
+        .then((result) => {
+            console.log(result);
+            res.sendStatus(201);
+        }).catch((error) => {
+            console.log(error);
+            res.sendStatus(500);
+        })
 });
 
 app.get('/song', (req, res) => {
